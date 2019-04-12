17b1e757a67712866ee116490793c5df392cd7e0
Neil Johnson <neil.johnson@thetradedesk.com>
2019-04-12 10:34:04 -0700

    fixed bash issue

---
+++ MacOfficeUninstall.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Type in user password"
+# Log in as Admin
+sudo -v
+
+cd /Applications
+sudo rm -rf Microsoft\ Excel.app
+sudo rm -rf Microsoft\ Outlook.app
+sudo rm -rf Microsoft\ PowerPoint.app
+sudo rm -rf Microsoft\ Word.app
+sudo rm -rf Microsoft\ OneDrive.app
+sudo rm -rf Microsoft\ OneNote.app
+
+cd /Library
+
+rm -rf com.microsoft.errorreporting
+rm -rf com.microsoft.Excel
+rm -rf com.microsoft.netlib.shipassertprocess
+rm -rf com.microsoft.Office365ServiceV2
+rm -rf com.microsoft.Outlook
+rm -rf com.microsoft.Powerpoint
+rm -rf com.microsoft.RMS-XPCService
+rm -rf com.microsoft.Word
+rm -rf com.microsoft.onenote.mac
+
+cd ~/Library/Group\ Containers
+
+rm -rf UBF8T346G9.ms
+rm -rf UBF8T346G9.Office
+rm -rf UBF8T346G9.OfficeOsfWebHost
+
+
+echo "Script Completed"
+
+
+
